114c39426dd391a3171986243e24982a03238a45
FROSTYTRIX <frostytrix13@gmail.com>
2026-05-16 11:48:15 +0200

    Updated to 26.1

--- src/main/java/net/frostytrix/elytratweaks/ClientModEvents.java
+++ src/main/java/net/frostytrix/elytratweaks/ClientModEvents.java
@@ -2,6 +2,7 @@ package net.frostytrix.elytratweaks;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -10,16 +11,24 @@ import org.lwjgl.glfw.GLFW;
 
 @EventBusSubscriber(modid = ElytraTweaksMod.MOD_ID, value = Dist.CLIENT)
 public class ClientModEvents {
+
+    public static final KeyMapping.Category ELYTRA_CATEGORY = new KeyMapping.Category(
+            Identifier.fromNamespaceAndPath(ElytraTweaksMod.MOD_ID, "controls")
+    );
+
     public static KeyMapping ELYTRA_DEPLOY_KEY;
 
     @SubscribeEvent
     public static void registerKeys(RegisterKeyMappingsEvent event) {
+        event.registerCategory(ELYTRA_CATEGORY);
+
         ELYTRA_DEPLOY_KEY = new KeyMapping(
-                "key.elytratweaks.deploy",
+                "key.elytra_tweaks.deploy",
                 InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_LEFT_ALT,
-                "category.elytratweaks"
+                GLFW.GLFW_KEY_G,
+                ELYTRA_CATEGORY
         );
+
         event.register(ELYTRA_DEPLOY_KEY);
     }
 }
