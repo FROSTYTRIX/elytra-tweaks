edecb8ca558a0d9420ffd385c0d86798ce513607
FROSTYTRIX <frostytrix13@gmail.com>
2026-05-16 22:32:50 +0200

    test number 4

--- src/main/java/net/frostytrix/elytratweaks/mixin/PlayerMixin.java
+++ src/main/java/net/frostytrix/elytratweaks/mixin/PlayerMixin.java
@@ -1,21 +1,26 @@
 package net.frostytrix.elytratweaks.mixin;
 
 import net.frostytrix.elytratweaks.ElytraTweaksState;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = Player.class, priority = 0)
-public abstract class PlayerEntityMixin {
+// Priority 1 ensures we are the absolute first to intercept this method call
+@Mixin(value = Player.class, priority = 1)
+public abstract class PlayerMixin {
 
     @Inject(method = "tryToStartFallFlying", at = @At("HEAD"), cancellable = true)
     private void onTryToStartFallFlying(CallbackInfoReturnable<Boolean> cir) {
-        LivingEntity entity = (LivingEntity) (Object) this;
-        if (entity.level().isClientSide() && entity instanceof Player) {
+        Player player = (Player) (Object) this;
+
+        // Ensure we only block it for the client-side player
+        if (player.level().isClientSide()) {
+
+            // If the deployment did not originate from our 'G' key
             if (!ElytraTweaksState.isCustomPress) {
+                // Cancel the method instantly. The elytra will not deploy.
                 cir.setReturnValue(false);
             }
         }
