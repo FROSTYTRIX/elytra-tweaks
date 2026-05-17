42c3d54a28ba8d84e2ed3b310bf0f2d4d0004798
FROSTYTRIX <frostytrix13@gmail.com>
2026-05-17 19:57:43 +0200

    Almost works for ATM10

--- src/main/java/net/frostytrix/elytratweaks/mixin/LocalPlayerMixin.java
+++ src/main/java/net/frostytrix/elytratweaks/mixin/LocalPlayerMixin.java
@@ -1,34 +1,55 @@
 package net.frostytrix.elytratweaks.mixin;
 
 import net.frostytrix.elytratweaks.ClientModEvents;
-import net.frostytrix.elytratweaks.ElytraTweaksState;
+import net.frostytrix.elytratweaks.ElytraTweaksModClient;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = LocalPlayer.class, priority = 0)
+@Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin {
+
+    // Tracks if the flight was started by your 'G' key
+    @Unique
+    private boolean elytraTweaks$isLegitFlight = false;
 
     @Inject(method = "aiStep", at = @At("HEAD"))
     private void onAiStepHead(CallbackInfo ci) {
         LocalPlayer player = (LocalPlayer) (Object) this;
 
-        while (ClientModEvents.ELYTRA_DEPLOY_KEY.consumeClick()) {
-            if (player.isFallFlying()) {
-                player.stopFallFlying();
+        // 1. Reset our legit state when you land or enter water
+        if (!player.isFallFlying()) {
+            this.elytraTweaks$isLegitFlight = false;
+        }
 
-                player.connection.send(new ServerboundPlayerCommandPacket(player, ServerboundPlayerCommandPacket.Action.START_FALL_FLYING));
-            } else {
-                ElytraTweaksState.isCustomPress = true;
-
-                if (player.tryToStartFallFlying()) {
+        // 2. Handle the Custom 'G' Key
+        if (ClientModEvents.ELYTRA_DEPLOY_KEY != null) {
+            while (ClientModEvents.ELYTRA_DEPLOY_KEY.consumeClick()) {
+                if (player.isFallFlying()) {
+                    // Turn OFF: We must manually send the toggle packet to stop
+                    player.stopFallFlying();
                     player.connection.send(new ServerboundPlayerCommandPacket(player, ServerboundPlayerCommandPacket.Action.START_FALL_FLYING));
+                    this.elytraTweaks$isLegitFlight = false;
+                } else {
+                    // Turn ON: tryToStartFallFlying() automatically sends the network packet for us!
+                    if (player.tryToStartFallFlying()) {
+                        this.elytraTweaks$isLegitFlight = true;
+                    }
                 }
-                ElytraTweaksState.isCustomPress = false;
             }
+        }
+
+        // 3. The Enforcer (The Anti-Spacebar Weapon)
+        // If the spacebar triggered flight, 'isLegitFlight' will be false.
+        if (player.isFallFlying() && !this.elytraTweaks$isLegitFlight) {
+            // Fold the wings instantly
+            player.stopFallFlying();
+            // Send the toggle packet to force the server to stop the spacebar flight too
+            player.connection.send(new ServerboundPlayerCommandPacket(player, ServerboundPlayerCommandPacket.Action.START_FALL_FLYING));
         }
     }
 }
